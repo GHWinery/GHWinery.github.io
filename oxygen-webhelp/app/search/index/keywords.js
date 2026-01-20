272bad311d83980c3ddead642f3621050c8de59c
Steve Helinski <helinskis@gmail.com>
2026-01-20 13:21:49 -0500

    Update

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,17 @@
+define(function() {var keywords=[{w:"Wine",p:["p0","p7"]},{w:"and",p:["p0","p1"]},{w:"Store",p:["p0","p5","p6","p7"]},{w:"Supply",p:["p0","p6"]},{w:"Orders",p:["p0","p6","p7","p8"]},{w:"Opening",p:["p1","p2","p3"]},{w:"the",p:["p1","p2","p3"]},{w:"Square",p:["p1"]},{w:"POS",p:["p1"]},{w:"Cash",p:["p1"]},{w:"Register",p:["p1"]},{w:"Winery",p:["p2","p3"]},{w:"Facility",p:["p2"]},{w:"Main",p:["p3"]},{w:"Ordering",p:["p4"]},{w:"Supplies",p:["p4"]},{w:"for",p:["p4","p7"]},{w:"Your",p:["p4"]},{w:"Location",p:["p4"]},{w:"Distributing",p:["p5"]},{w:"Order",p:["p5"]},{w:"Forms",p:["p5"]},{w:"Fulfiling",p:["p6","p7"]},{w:"to",p:["p6"]},{w:"Locations",p:["p6","p7"]},{w:"Receiving",p:["p8"]},{w:"at",p:["p8"]},{w:"Stores",p:["p8"]},{w:"Welcome",p:["p9"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4];
+ph["p1"]=[5, 6, 7, 8, 1, 9, 10];
+ph["p2"]=[5, 6, 11, 12];
+ph["p3"]=[5, 6, 13, 11];
+ph["p4"]=[14, 15, 16, 17, 18];
+ph["p5"]=[19, 2, 20, 21];
+ph["p6"]=[22, 3, 4, 23, 2, 24];
+ph["p7"]=[22, 0, 4, 16, 2, 24];
+ph["p8"]=[25, 4, 26, 27];
+ph["p9"]=[28];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {4:0,1:3,6:0,2:3,3:-1,5:0,8:0,0:-1,9:-1,7:0};
+});

e6619db7a28f7cdb5ebc1cf77842b59ab4259ef9
Steve Helinski <helinskis@gmail.com>
2026-01-20 16:29:46 -0500

    Removed all

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,15 +1,17 @@
-define(function() {var keywords=[{w:"Wine",p:["p0","p7"]},{w:"and",p:["p0","p1"]},{w:"Store",p:["p0","p5","p6","p7"]},{w:"Supply",p:["p0","p6"]},{w:"Orders",p:["p0","p6","p7","p8"]},{w:"Opening",p:["p1","p2","p3"]},{w:"the",p:["p1","p2","p3"]},{w:"Square",p:["p1"]},{w:"POS",p:["p1"]},{w:"Cash",p:["p1"]},{w:"Register",p:["p1"]},{w:"Winery",p:["p2","p3"]},{w:"Facility",p:["p2"]},{w:"Main",p:["p3"]},{w:"Ordering",p:["p4"]},{w:"Supplies",p:["p4"]},{w:"for",p:["p4","p7"]},{w:"Your",p:["p4"]},{w:"Location",p:["p4"]},{w:"Distributing",p:["p5"]},{w:"Order",p:["p5"]},{w:"Forms",p:["p5"]},{w:"Fulfiling",p:["p6","p7"]},{w:"to",p:["p6"]},{w:"Locations",p:["p6","p7"]},{w:"Receiving",p:["p8"]},{w:"at",p:["p8"]},{w:"Stores",p:["p8"]},{w:"Welcome",p:["p9"]}];
+define(function() {var keywords=[{w:"Distributing",p:["p0"]},{w:"Store",p:["p0","p1","p2","p5","p10","p11"]},{w:"Order",p:["p0"]},{w:"Forms",p:["p0"]},{w:"Fulfilling",p:["p1","p2"]},{w:"Supply",p:["p1","p5","p10"]},{w:"Orders",p:["p1","p2","p4","p5","p10","p11"]},{w:"to",p:["p1","p10"]},{w:"Locations",p:["p1","p2","p10","p11"]},{w:"Wine",p:["p2","p5","p11"]},{w:"for",p:["p2","p3","p11"]},{w:"Ordering",p:["p3"]},{w:"Supplies",p:["p3"]},{w:"Your",p:["p3"]},{w:"Location",p:["p3"]},{w:"Receiving",p:["p4"]},{w:"at",p:["p4"]},{w:"Stores",p:["p4"]},{w:"and",p:["p5","p6"]},{w:"Opening",p:["p6","p7","p8"]},{w:"the",p:["p6","p7","p8"]},{w:"Square",p:["p6"]},{w:"POS",p:["p6"]},{w:"Cash",p:["p6"]},{w:"Register",p:["p6"]},{w:"Winery",p:["p7","p8"]},{w:"Facility",p:["p7"]},{w:"Main",p:["p8"]},{w:"Welcome",p:["p9"]},{w:"Fulfiling",p:["p10","p11"]}];
 var ph={};
-ph["p0"]=[0, 1, 2, 3, 4];
-ph["p1"]=[5, 6, 7, 8, 1, 9, 10];
-ph["p2"]=[5, 6, 11, 12];
-ph["p3"]=[5, 6, 13, 11];
-ph["p4"]=[14, 15, 16, 17, 18];
-ph["p5"]=[19, 2, 20, 21];
-ph["p6"]=[22, 3, 4, 23, 2, 24];
-ph["p7"]=[22, 0, 4, 16, 2, 24];
-ph["p8"]=[25, 4, 26, 27];
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[4, 5, 6, 7, 1, 8];
+ph["p11"]=[29, 9, 6, 10, 1, 8];
+ph["p2"]=[4, 9, 6, 10, 1, 8];
+ph["p3"]=[11, 12, 10, 13, 14];
+ph["p4"]=[15, 6, 16, 17];
+ph["p5"]=[9, 18, 1, 5, 6];
+ph["p6"]=[19, 20, 21, 22, 18, 23, 24];
+ph["p7"]=[19, 20, 25, 26];
+ph["p8"]=[19, 20, 27, 25];
 ph["p9"]=[28];
+ph["p10"]=[29, 5, 6, 7, 1, 8];
      return {
          keywords: keywords,
          ph: ph
--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {4:0,1:3,6:0,2:3,3:-1,5:0,8:0,0:-1,9:-1,7:0};
+return {14:10,11:13,16:10,12:13,13:-1,15:10,18:10,10:-1,19:-1,17:10};
 });

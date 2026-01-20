2113e4b80f0e77593b997f685a10a177b30db92a
Steve Helinski <helinskis@gmail.com>
2026-01-20 16:30:32 -0500

    Remove

--- oxygen-webhelp/app/search/index/link-to-parent.js
+++
@@ -1,4 +0,0 @@
-/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-define(function () {
-return {14:10,11:13,16:10,12:13,13:-1,15:10,18:10,10:-1,19:-1,17:10};
-});
